e211ed95dcc270ff426f0f67e04ae484d8dd1b6f
Kieran Canavan <kieran.canavan@clarivate.com>
2021-09-09 18:02:29 +0200

    Constructed Title for note
    Signed-off-by: Kieran Canavan <kieran.canavan@clarivate.com>

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ func main() {
 
 	weekday := time.Now().Weekday()
 	current_time := time.Now().Format(time.Kitchen)
+	year, week_number := time.Now().UTC().ISOWeek()
 
 	var message = flag.String("message", " ", "Todo item")
 	flag.Parse()
 
+
+	var title = fmt.Sprintf("Week %d %d Notes\n", week_number, year)
+
+	fmt.Printf(title)
 	fmt.Println("Weekday: ", weekday)
 	fmt.Printf("%s @ %s\n", *message, current_time)
 }
