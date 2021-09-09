00048d39b20f236d9ef5bce550f5eb0834a79578
Kieran Canavan <kieran.canavan@clarivate.com>
2021-09-09 18:37:55 +0200

    Adding output file
    Signed-off-by: Kieran Canavan <kieran.canavan@clarivate.com>

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"log"
 	"flag"
 	"fmt"
 	"time"
+	"os"
 )
+
+	
+func check(e error) {
+	if e != nil {
+			panic(e)
+	}
+}
+
+func ensure_dir(dir string) {
+	_, err := os.Stat(dir)
+ 
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(dir, 0755)
+		if errDir != nil {
+			log.Fatal(err)
+		}
+ 
+	}
+}
+
 
 func main() {
 
@@ -12,7 +34,7 @@ func main() {
 	current_time := time.Now().Format(time.Kitchen)
 	year, week_number := time.Now().UTC().ISOWeek()
 
-	var message = flag.String("message", " ", "Todo item")
+	var message = flag.String("message", "<message goes here>", "Todo item")
 	flag.Parse()
 
 
@@ -21,4 +43,15 @@ func main() {
 	fmt.Printf(title)
 	fmt.Println("Weekday: ", weekday)
 	fmt.Printf("%s @ %s\n", *message, current_time)
+
+	ensure_dir("files")
+
+
+	// Write file 
+	f, err := os.Create("files/sample.md")
+	check(err)
+
+	// ensure Close is performed later in the program’s execution, cleanup
+	defer f.Close()
+
 }
